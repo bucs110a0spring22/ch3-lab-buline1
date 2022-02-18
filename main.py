be5bf61b912ca866d02f8afc166a11cccf3b1f75
Benjamin Uline J <98502703+buline1@users.noreply.github.com>
2022-02-18 04:36:32 +0000

    Finished coding of lab 3

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import turtle               #1. import modules
 import random
 
 #Part A
-window = turtle.Screen()       # 2.  Create a screen
+window = turtle.Screen() 
+window.setup(width=300, height=200)
+# 2.  Create a screen
 window.bgcolor('lightblue')
 
 michelangelo = turtle.Turtle()    # 3.  Create two turtles
@@ -18,9 +20,56 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+## Race 1
+michelangelo.speed(1)
+leonardo.speed(1)
+x = random.randrange(1,101)
+y = random.randrange(1,101)
+michelangelo.forward(x)
+leonardo.forward(y)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+## Race 2
+for i in range(10):
+  x_2 = random.randrange(1,11)
+  michelangelo.forward(x_2)
+  y_2 = random.randrange(1,11)
+  leonardo.forward(y_2)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 # Part B - complete part B here
-
-
+michelangelo.forward(50)
+michelangelo.pendown()
+##Triangle
+sides = 3
+for i in range(sides):
+  michelangelo.left(360/sides)
+  michelangelo.forward(60)
+michelangelo.clear()
+##Square
+sides = 4
+for i in range(sides):
+  michelangelo.left(360/sides)
+  michelangelo.forward(60)
+michelangelo.clear()
+##Hexagon
+sides = 6
+for i in range(sides):
+  michelangelo.left(360/sides)
+  michelangelo.forward(60)
+michelangelo.clear()
+##Nonagon
+sides = 9
+for i in range(sides):
+  michelangelo.left(360/sides)
+  michelangelo.forward(60)
+michelangelo.clear()
+##Dodecagon
+sides = 12
+for i in range(sides):
+  michelangelo.left(360/sides)
+  michelangelo.forward(60)
+michelangelo.clear()
 window.exitonclick()
